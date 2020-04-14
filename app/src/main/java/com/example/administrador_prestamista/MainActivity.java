23c47d6263cd5810475bc062f1c92540e9fedae6
andresreyes22 <62660786+andresreyes22@users.noreply.github.com>
2020-04-13 21:19:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/administrador_prestamista/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrador_prestamista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
